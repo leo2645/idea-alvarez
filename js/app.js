8acc592432aee850bf084c702e9b7230e1e367dc
Leonardo Alvarez <leo_609@hotmail.com>
2022-09-03 11:11:45 -0300

    Entrega-final-javascript

--- js/app.js
+++
@@ -1,7 +0,0 @@
-const links = document.querySelectorAll('[data-link]')
-
-links.forEach((link)=>{
-    link.addEventListener('click',() =>{
-        link.setAttribute('aria-current',page)
-    })
-})
